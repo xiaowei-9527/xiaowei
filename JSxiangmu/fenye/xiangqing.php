e08269e41535f0ecafe732d4f0961916d3665e75
xiaowei <1729621924@qq.com>
2019-12-26 20:37:13 +0800

    面向对象与原型+匿名函数和闭包

---
+++ JSxiangmu/fenye/xiangqing.php
@@ -0,0 +1,25 @@
+<?php
+	header("content-type:text/html;charset=utf-8");
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	$dbname = "html";
+	$id=$_GET['id'];
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	// 检测连接
+	if ($conn->connect_error) {
+	    die("连接失败: " . $conn->connect_error);
+	} 
+	$sql1 = "SELECT * FROM newslist where newsid=$id";
+
+	mysqli_query($conn, "set names utf8");
+	
+	$result1=$conn->query($sql1);
+
+	
+	while($row=$result1->fetch_object()){
+		echo json_encode(array('code'=>200,'data'=>$row));
+  }
+  	$conn->close();
+?>
