66b2537873cd0e4ab8efbecfec904c59a69b35a3
xiaowei <1729621924@qq.com>
2020-01-13 20:55:53 +0800

    JQ文档完结

---
+++ JQxiangmu/JQword/weituo.php
@@ -0,0 +1,12 @@
+<?php
+	echo json_encode(
+		array(
+			array('id'=>1,'title'=>'1111'),
+			array('id'=>2,'title'=>'2222'),
+			array('id'=>3,'title'=>'3333'),
+			array('id'=>4,'title'=>'4444')
+		)
+	);
+?>
+
+
